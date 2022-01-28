ae2f192f5024f66f2e4cbc18254872e52049ac84
frogfrequency <braun.marcel@hotmail.com>
2022-01-28 10:17:41 +0100

    testing gitignore for angular

--- backend/routes/monsters.ts
+++ backend/routes/monsters.ts
@@ -6,6 +6,7 @@ import * as monsterService from "../services/monster.service";
 export const monsterRouter = Router();
 
 monsterRouter.get('/', async (req: Request, res: Response) => {
+    console.log('/ (allMonsters) called');
     const allMonsters: IMonster[] = await monsterService.fetchAllMonsters();
     res.json(allMonsters);
 });
@@ -18,10 +19,27 @@ monsterRouter.get('/', async (req: Request, res: Response) => {
 //     });
 // })
 
+monsterRouter.get('/filter', async (req: Request, res: Response) => {
+    var query = require('url').parse(req.url,true).query; // SANDROSANDROSANDRO https://stackoverflow.com/questions/6912584/how-to-get-get-query-string-variables-in-express-js-on-node-js   Peter Mortensen 86 upvotes
+    const filterProperty: string = query.filterProperty;
+    const filterValue = query.filterValue;
+    const monstersWithSpecificFilter: IMonster[] = await monsterService.fetchAllMonstersWithSpecificFilter(filterProperty, filterValue);
+    res.json(monstersWithSpecificFilter);
+}); 
+
 monsterRouter.get('/:name', async (req: Request, res: Response) => {
+    console.log('/:name called');
     const monsterName: string = req.params.name;
     const monster: IMonster = await monsterService.fetchMonsterByName(monsterName);
     res.json(monster);
 });
+
+monsterRouter.get('/type/:type', async (req: Request, res: Response) => { // is theoretically not needed anymore because you can use the '/stringfilter'-route to filter for types etc..
+    const typeName = req.params.type;
+    const allMonsters: IMonster[] = await monsterService.fetchAllMonsters();
+    const allMonstersWithSpecifiedType: IMonster[] = allMonsters.filter(monster => monster.type === typeName)
+    res.json(allMonstersWithSpecifiedType);
+}); 
+
 
 // module.exports = monsterRouter;
--- backend/services/monster.service.ts
+++ backend/services/monster.service.ts
@@ -4,13 +4,54 @@ import { IAllMonstersApiResponse, IMonster } from '../interfaces/models/IMonster
 const monsterApiBaseURL = 'https://api.open5e.com/monsters/'
 
 export const fetchAllMonsters = async (): Promise<IMonster[]> => {
-    const apiResponse = await axios.get(monsterApiBaseURL);
-    const allMonstersResponse: IAllMonstersApiResponse = apiResponse.data;
-    return allMonstersResponse.results;
+    let allMonsters: IMonster[] = []; // the api does only return the first page (50 monsters) and a next url. we have to call all the pages and collect all the monsters
+    let apiResponse = await axios.get(monsterApiBaseURL);
+    let allMonstersResponse: IAllMonstersApiResponse = apiResponse.data;
+    let next = allMonstersResponse.next;
+
+    allMonstersResponse.results.forEach(monster => {
+        allMonsters.push(monster)
+    })
+
+    while(next) {
+            apiResponse = await axios.get(next);
+            allMonstersResponse = apiResponse.data;
+            allMonstersResponse.results.forEach(monster => {
+                allMonsters.push(monster)
+            })
+            next = allMonstersResponse.next
+        }
+
+    return allMonsters;
 }
 
 export const fetchMonsterByName = async (name: string): Promise<IMonster> => {
     const apiResponse = await axios.get(monsterApiBaseURL + name);
     const monsterResponse: IMonster = apiResponse.data;
     return monsterResponse;
+}
+
+export const fetchAllMonstersPageOne = async (): Promise<IMonster[]> => {
+    const apiResponse = await axios.get(monsterApiBaseURL);
+    const allMonstersResponse: IAllMonstersApiResponse = apiResponse.data;
+    return allMonstersResponse.results;
+}
+
+export const fetchAllMonstersWithSpecificFilter = async (filterProperty: string, filterValue: string): Promise<IMonster[]> => {
+    const allMonsters: IMonster[] = await fetchAllMonsters();
+    let allMonstersWithFilterApplied: IMonster[] = [];
+    allMonsters.forEach(monster => {
+        let propertyType: string = typeof(monster[filterProperty as keyof IMonster]);
+
+        if (propertyType === 'string') {
+            if (monster[filterProperty as keyof IMonster] === filterValue) {
+                allMonstersWithFilterApplied.push(monster);
+            }
+        } else {
+            if (JSON.stringify(monster[filterProperty as keyof IMonster]) === filterValue) {
+                allMonstersWithFilterApplied.push(monster);
+            }
+        }
+    })
+    return allMonstersWithFilterApplied
 }
