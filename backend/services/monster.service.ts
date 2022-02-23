0564618569595d46ccd2a151c718a3fabf0091cd
frogfrequency <braun.marcel@hotmail.com>
2022-02-23 20:08:18 +0100

    working on monsterpage

--- backend/services/monster.service.ts
+++ backend/services/monster.service.ts
@@ -4,31 +4,40 @@ import { IAllMonstersApiResponse, IMonster } from '../interfaces/models/IMonster
 
 const monsterApiBaseURL = 'https://api.open5e.com/monsters/'
 
+// this fetchAllMonstersFunction was replaced by a better one. it is stored for possible later partial reuse 
+// export const fetchAllMonsters = async (): Promise<IMonster[]> => {
+//     let allMonsters: IMonster[] = []; // the api does only return the first page (50 monsters) and a next url. we have to call all the pages and collect all the monsters
+//     let apiResponse = await axios.get(monsterApiBaseURL);
+//     let allMonstersResponse: IAllMonstersApiResponse = apiResponse.data;
+//     let next = allMonstersResponse.next;
+
+//     allMonstersResponse.results.forEach(monster => {
+//         allMonsters.push(monster)
+//     })
+
+//     while (next) {
+//         apiResponse = await axios.get(next);
+//         allMonstersResponse = apiResponse.data;
+//         allMonstersResponse.results.forEach(monster => {
+//             allMonsters.push(monster)
+//         })
+//         next = allMonstersResponse.next
+//     }
+
+//     return allMonsters;
+// }
+
+
 export const fetchAllMonsters = async (): Promise<IMonster[]> => {
-    let allMonsters: IMonster[] = []; // the api does only return the first page (50 monsters) and a next url. we have to call all the pages and collect all the monsters
-    let apiResponse = await axios.get(monsterApiBaseURL);
-    let allMonstersResponse: IAllMonstersApiResponse = apiResponse.data;
-    let next = allMonstersResponse.next;
-
-    allMonstersResponse.results.forEach(monster => {
-        allMonsters.push(monster)
-    })
-
-    while (next) {
-        apiResponse = await axios.get(next);
-        allMonstersResponse = apiResponse.data;
-        allMonstersResponse.results.forEach(monster => {
-            allMonsters.push(monster)
-        })
-        next = allMonstersResponse.next
-    }
-
+    let apiResponse = await axios.get(monsterApiBaseURL+ '?limit=2000');
+    // let allMonstersResponse: IAllMonstersApiResponse = apiResponse.data;
+    // let next = allMonstersResponse.next;
+    let allMonsters: IMonster[] = apiResponse.data.results;
     return allMonsters;
 }
 
 export const fetchMonsterByName = async (name: string): Promise<IMonster | number> => {
-    console.log('before');
-
+    
     const apiResponse = await axios.get(monsterApiBaseURL + name)
         .catch(error => { return error.response.status });
     
