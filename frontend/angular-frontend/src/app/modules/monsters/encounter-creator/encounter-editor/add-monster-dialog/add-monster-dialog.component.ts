b6e8d9004dffc35310b63c984dcbfea3f0c13e91
frogfrequency <66001719+frogfrequency@users.noreply.github.com>
2022-03-08 17:13:43 +0100

    finished task: adding monster from favorites to encounter (#6)

--- frontend/angular-frontend/src/app/modules/monsters/encounter-creator/encounter-editor/add-monster-dialog/add-monster-dialog.component.ts
+++ frontend/angular-frontend/src/app/modules/monsters/encounter-creator/encounter-editor/add-monster-dialog/add-monster-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { IMonster } from 'src/app/interfaces/IMonster';
+import { EncounterService } from '../../../services/encounter.service';
 import { FavoritesService } from '../../../services/favorites.service';
 
 @Component({
@@ -11,21 +12,25 @@ import { FavoritesService } from '../../../services/favorites.service';
 export class AddMonsterDialogComponent implements OnInit {
 
   favoritedMonsters: IMonster[] = [];
-  currentlyFavoritedMonster: IMonster | undefined = undefined; 
+  monstersAlreadyInEncounter: string[] = [];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private favoritesService: FavoritesService) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private favoritesService: FavoritesService, private encounterService: EncounterService) { }
 
   ngOnInit(): void {
     this.favoritesService.favoritedMonstersSubject.subscribe(favorites => {
       this.favoritedMonsters = favorites;
+      this.data.encounter.monsters.forEach( (monster: any) => {
+        this.monstersAlreadyInEncounter.push(monster.slug)
+      })
     });
   }
 
-  setSelectedMonster(slug: string): void {
-    // if (this.favoritedMonsters) {
-      console.log(`setSelectedMonster is called with: ${slug}`)
-      this.currentlyFavoritedMonster = this.favoritedMonsters.find(element => element.slug === slug)
-    // }
-  }
+
+  // setSelectedMonster(slug: string): void {
+  //   // if (this.favoritedMonsters) {
+  //     console.log(`setSelectedMonster is called with: ${slug}`)
+  //     this.currentlyFavoritedMonster = this.favoritedMonsters.find(element => element.slug === slug)
+  //   // }
+  // }
 
 }
